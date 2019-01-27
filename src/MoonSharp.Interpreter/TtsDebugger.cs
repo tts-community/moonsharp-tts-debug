8cc6b6d0aef75f69a5ea6ac787199f959ab899f2
Benjamin Dobell <benjamin.dobell+git@glassechidna.com.au>
2019-01-28 01:36:29 +1100

    Tabs not spaces

--- src/MoonSharp.Interpreter/TtsDebugger.cs
+++ src/MoonSharp.Interpreter/TtsDebugger.cs
@@ -4,51 +4,51 @@ using MoonSharp.VsCodeDebugger;
 
 namespace MoonSharp.Interpreter
 {
-    public static class TtsDebugger
-    {
-        private static MoonSharpVsCodeDebugServer server;
+	public static class TtsDebugger
+	{
+		private static MoonSharpVsCodeDebugServer server;
 
-        public static MoonSharpVsCodeDebugServer GetServer()
-        {
-            if (server == null)
-            {
-                server = new MoonSharpVsCodeDebugServer();
-                server.Start();
-            }
+		public static MoonSharpVsCodeDebugServer GetServer()
+		{
+			if (server == null)
+			{
+				server = new MoonSharpVsCodeDebugServer();
+				server.Start();
+			}
 
-            return server;
-        }
+			return server;
+		}
 
-        public static string GetScriptName(Type luaScriptType, Script script)
-        {
-            Type baseLuaScriptType = luaScriptType.BaseType;
+		public static string GetScriptName(Type luaScriptType, Script script)
+		{
+			Type baseLuaScriptType = luaScriptType.BaseType;
 
-            if (baseLuaScriptType != null)
-            {
-                MethodInfo scriptToLuaScriptMethod = baseLuaScriptType.GetMethod("ScriptToLuaScript");
+			if (baseLuaScriptType != null)
+			{
+				MethodInfo scriptToLuaScriptMethod = baseLuaScriptType.GetMethod("ScriptToLuaScript");
 
-                if (scriptToLuaScriptMethod != null)
-                {
-                    object luaScript = scriptToLuaScriptMethod.Invoke(null, new object[] {script});
+				if (scriptToLuaScriptMethod != null)
+				{
+					object luaScript = scriptToLuaScriptMethod.Invoke(null, new object[] {script});
 
-                    if (luaScript != null)
-                    {
-                        MethodInfo getScriptNameMethod = luaScript.GetType().GetMethod("GetScriptName");
+					if (luaScript != null)
+					{
+						MethodInfo getScriptNameMethod = luaScript.GetType().GetMethod("GetScriptName");
 
-                        if (getScriptNameMethod != null)
-                        {
-                            object name = getScriptNameMethod.Invoke(luaScript, new object[] { });
+						if (getScriptNameMethod != null)
+						{
+							object name = getScriptNameMethod.Invoke(luaScript, new object[] { });
 
-                            if (name != null)
-                            {
-                                return Convert.ToString(name);
-                            }
-                        }
-                    }
-                }
-            }
+							if (name != null)
+							{
+								return Convert.ToString(name);
+							}
+						}
+					}
+				}
+			}
 
-            return null;
-        }
-    }
+			return null;
+		}
+	}
 }
